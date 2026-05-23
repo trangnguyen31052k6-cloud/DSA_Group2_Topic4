053152ad1747f385288bd6bd53ab7ad54c52b148
Nguyễn Đình Khải <73068567+DinhKhaineu@users.noreply.github.com>
2026-05-24 00:32:45 +0700

    Implement insert, backspace, and enter functionality

--- Insert_and_Delete/insertAndDelete.py
+++ Insert_and_Delete/insertAndDelete.py
@@ -1,1 +1,67 @@
+class InsertAndDelete:
+    def __init__(self,document, cursor):
+        self.document = document
+        self.cursor = cursor
 
+    def Insert_at_cursor(self, text):
+        current_line = self.cursor.current_node.data
+        position = self.cursor.col_index
+
+        left = current_line[:position]
+        right = current_line[position:]
+
+        new_line = left + text + right
+
+        self.cursor.current_node.data = new_line
+
+        self.cursor.col_index = position + len(text)
+
+    def Backspace_key(self):
+        current_line = self.cursor.current_node.data
+        position = self.cursor.col_index
+
+        if self.cursor.col_index == 0 and self.cursor.current_node.prev:
+            
+            prev_node = self.cursor.current_node.prev
+            future_position = len(prev_node.data)
+
+            curr_node = self.cursor.current_node
+
+            self.cursor.current_node.prev.data += curr_node.data
+
+            self.document.remove_node(curr_node) 
+            self.cursor.current_node = prev_node
+
+            self.cursor.col_index = future_position
+            
+        elif position == 0:
+            return
+        
+        else:
+            left = current_line[:position - 1]
+            right = current_line[position:]
+
+            new_line = left + right
+            self.cursor.current_node.data = new_line
+            
+            self.cursor.col_index -= 1
+
+
+    def Enter_key(self):
+        current_line = self.cursor.current_node.data
+        position = self.cursor.col_index
+
+        left = current_line[:position]
+        right = current_line[position:]
+
+        self.cursor.current_node.data = left
+        self.document.insert_after_node(self.cursor.current_node, right)
+
+        self.cursor.move_down()
+        self.cursor.col_index = 0
+
+    
+
+
+
+    
